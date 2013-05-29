bb2e47d5a088075e30c1e1e74016210accd55476
Ryan Cerniglia <pz@programzeta.com>
2013-05-29 12:59:10 -0700

    Adds tests for the long names of previous arguments

--- tests/xmlsrr-test.py
+++ tests/xmlsrr-test.py
@@ -32,9 +32,21 @@ class TestInitialization(unittest.TestCase):
         args = xmlsrr.argument_parser(arguments)
         self.assertEqual(args.instructions, instructionFile)
 
+    def test_instruction_file_argument_long_name(self):
+        instructionFile = '/tmp/test-instructions.txt'
+        arguments = '/tmp/xmlsrr-test --instructions ' + instructionFile
+        args = xmlsrr.argument_parser(arguments)
+        self.assertEqual(args.instructions, instructionFile)
+
     def test_log_file_argument(self):
         logFile = '/tmp/test-log.log'
         arguments = '/tmp/xmlsrr-test -l ' + logFile
+        args = xmlsrr.argument_parser(arguments)
+        self.assertEqual(args.log, logFile)
+
+    def test_log_file_argument_long_name(self):
+        logFile = '/tmp/test-log.log'
+        arguments = '/tmp/xmlsrr-test --log ' + logFile
         args = xmlsrr.argument_parser(arguments)
         self.assertEqual(args.log, logFile)
 
@@ -49,8 +61,19 @@ class TestInitialization(unittest.TestCase):
         args = xmlsrr.argument_parser(arguments)
         self.assertEqual(args.output, outputFolder)
 
+    def test_output_folder_argument_long_name(self):
+        outputFolder = '/tmp/xmlsrr/output/'
+        arguments = '/tmp/xmlsrr/source/ --output ' + outputFolder
+        args = xmlsrr.argument_parser(arguments)
+        self.assertEqual(args.output, outputFolder)
+
     def test_silent_argument(self):
         arguments = '/tmp/xmlsrr/source/ -s'
+        args = xmlsrr.argument_parser(arguments)
+        self.assertTrue(args.silent)
+
+    def test_silent_argument_long_name(self):
+        arguments = '/tmp/xmlsrr/source/ --silent'
         args = xmlsrr.argument_parser(arguments)
         self.assertTrue(args.silent)
 
