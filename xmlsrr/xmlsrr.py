f5231c3688d1cc0e1758e92efd61eff7411b1cd7
Ryan Cerniglia <pz@programzeta.com>
2013-05-29 12:54:11 -0700

    Adds '-v' parameter

--- xmlsrr/xmlsrr.py
+++ xmlsrr/xmlsrr.py
@@ -13,6 +13,7 @@ def argument_parser(arguments):
     parser.add_argument('-l', '--log')
     parser.add_argument('-o', '--output')
     parser.add_argument('-s', '--silent', action='store_true')
+    parser.add_argument('-v', action='count')
     return parser.parse_args(args=arguments.split())
 
 if __name__ == '__main__':
