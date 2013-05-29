f58065f2c5fd696232173985b08c248c9ba0a2dc
Ryan Cerniglia <pz@programzeta.com>
2013-05-29 12:35:56 -0700

    Specifes '-i' parameter named as 'instructions'

--- xmlsrr/xmlsrr.py
+++ xmlsrr/xmlsrr.py
@@ -9,7 +9,7 @@ def argument_parser(arguments):
     parser = argparse.ArgumentParser(description='Scans files in a folder to find matches, remove, \
     and/or replace elements based off of CSS-like syntax', prog='xmlsrr')
     parser.add_argument('target', type=str)
-    parser.add_argument('-i')
+    parser.add_argument('-i', '--instructions')
     parser.add_argument('-l', '--log')
     return parser.parse_args(args=arguments.split())
 
